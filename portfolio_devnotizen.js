e89ed7141dcbfb0f100a8220a8ea26db1c8d6970
WKD-Development <112250436+wkddevelopment@users.noreply.github.com>
2022-10-08 21:13:52 +0200

    devnotizen

---
+++ portfolio_devnotizen.js
@@ -0,0 +1,26 @@
+// ? DEV-Notizen WKD-PORTFOLIO - Next.js
+
+// ? Allgemein
+// todo ==> Title einbauen! (Translation) Navbra nicht vergessen!
+// todo ==> Pseudoelements auf allen Seiten angleichen
+// todo ==> CSS-rules for different device-sizes
+// todo ==> CSS-Code cleaning
+// todo ==> JS-Code cleaning
+// todo ==> Alle Texte durchsehen und formatieren
+// todo ==> README
+REMOVE ALKL DATA NOT USED
+// ? Navbar
+// todo ==>
+
+// ? Home
+// todo ==>
+
+// ? About
+// todo ==> Erklärungen zur Translation, den Animation für jeden die es abgeschalten haben
+// todo ==>
+
+// ? Contact
+// todo ==>
+
+// ? Project
+// todo ==>
